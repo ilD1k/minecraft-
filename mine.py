de2e3c0666495dfb1b904cb4fb5a07922c5e3908
ilD1k <151182252+ilD1k@users.noreply.github.com>
2024-01-28 21:04:42 +0500

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,3 +1,143 @@
+
+
+
+
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
 
 app = Ursina()
+
+
+
+
+
+
+grass_texture = load_texture('assets/grass.png')
+dirt_texture = load_texture('assets/dirt.png')
+oak_texture = load_texture('assets/oak.png')
+send_texture = load_texture('assets/send.png')
+stone_texture = load_texture('assets/stone.png')
+wood_texture = load_texture('assets/wood.png')
+sky_texture = load_texture('assets/sky.png')
+
+current_texture = grass_texture
+
+
+def update():
+	global current_texture
+	if held_keys['1']:
+		current_texture = grass_texture
+
+	if held_keys['2']:
+		current_texture = dirt_texture
+
+	if held_keys['3']:
+		current_texture = oak_texture
+
+	if held_keys['4']:
+		current_texture = send_texture
+
+	if held_keys['5']:
+		current_texture = stone_texture
+
+	if held_keys['6']:
+		current_texture = wood_texture
+
+	if held_keys["7"]:
+		current_texture = sky_texture
+
+	if held_keys['left mouse'] or held_keys['right mouse']:
+		hand.active()
+	else:
+		hand.passive()
+
+
+
+
+
+
+#Создание неба
+class Sky(Entity):
+	def __init__(self):
+		super().__init__(
+				parent = scene,
+				model = 'sphere',
+				scale = 150,
+				texture	= sky_texture,
+				double_sided = True
+			)
+
+
+
+
+
+
+#создание и анимация руки
+class Hand(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = camera.ui,
+			model = 'cube',
+			scale = (0.2, 0.3),
+			color = color.white,
+			rotation = Vec3(150, -10, 0),
+			position = Vec2(0.4, -0.4)
+			)
+
+	def active(self):
+		self.position = Vec2(0.1, -0.5)
+		self.rotation = Vec3(90, -10, 0)
+
+	def passive(self):
+		self.rotation = Vec3(150, -10, 0)
+		self.position = Vec2(0.4, -0.4)
+
+
+
+
+
+
+
+class Voxel(Button):
+	def __init__(self, position=(0, 0, 0), texture = grass_texture):
+		super().__init__(
+			parent=scene,
+			position=position,
+			model='cube',
+			origin_y=.5,
+			texture=texture,
+			color=color.color(0, 0, 255),
+			highlight_color = color.lime
+			)
+
+
+
+
+
+	def input(self, key):
+		if self.hovered:
+			if key == 'right mouse down':
+				voxel = Voxel(position = self.position + mouse.normal, texture = current_texture)
+			if key == 'left mouse down':
+				destroy(self)
+
+
+
+
+
+
+for z in range(15):
+	for x in range(15):
+		voxel = Voxel((x, 0, z))
+
+
+
+player = FirstPersonController()
+sky = Sky()
+hand = Hand()
+
+app.run()
+
+
+
